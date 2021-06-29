c23a7786c5a5e3955e2ef2d458dd1d876190c042
Martin Brennan <martin@discourse.org>
2021-06-30 08:54:00 +1000

    Uppy working in custom bundle

--- app/assets/javascripts/vendor.js
+++ app/assets/javascripts/vendor.js
@@ -24,6 +24,7 @@
 //= require show-html.js
 //= require buffered-proxy
 //= require jquery.autoellipsis-1.0.10
+//= require uppy.js
 //= require virtual-dom
 //= require virtual-dom-amd
 //= require discourse-shims
