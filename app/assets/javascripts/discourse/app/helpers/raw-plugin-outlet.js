7893073ebf7928d37729f09f01b4a1eb87a9f2c5
David Taylor <david@taylorhq.com>
2022-01-24 18:17:48 +0000

    DEV: Allow raw-plugin-outlet to be rendered in non-raw templates
    This will allow us to switch templates to Glimmer without immediately breaking any themes/plugins which use raw plugin outlets. When used in Glimmer/Ember templates, the 'context' must be supplies explicitly.

--- app/assets/javascripts/discourse/app/helpers/raw-plugin-outlet.js
+++ app/assets/javascripts/discourse/app/helpers/raw-plugin-outlet.js
@@ -1,11 +1,12 @@
-import RawHandlebars from "discourse-common/lib/raw-handlebars";
 import { htmlSafe } from "@ember/template";
 import { rawConnectorsFor } from "discourse/lib/plugin-connectors";
+import { registerUnbound } from "discourse-common/lib/helpers";
 
-RawHandlebars.registerHelper("raw-plugin-outlet", function (args) {
-  const connectors = rawConnectorsFor(args.hash.name);
+registerUnbound("raw-plugin-outlet", function (params) {
+  const connectors = rawConnectorsFor(params.name);
+  const context = params.context || this; // 'this' is used in raw-template environments. 'params.context' is used in Glimmer contexts
   if (connectors.length) {
-    const output = connectors.map((c) => c.template({ context: this }));
+    const output = connectors.map((c) => c.template({ context }));
     return htmlSafe(output.join(""));
   }
 });
