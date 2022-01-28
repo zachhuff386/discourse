2f44092236164e9e28c4059ae3d1e00416b8d71a
David Taylor <david@taylorhq.com>
2022-01-28 16:50:16 +0000

    DEV: Update plugin-outlet to Glimmer component
    For now, the individual connectors are still classic components

--- app/assets/javascripts/discourse/app/components/plugin-outlet.js
+++ app/assets/javascripts/discourse/app/components/plugin-outlet.js
@@ -2,6 +2,8 @@ import {
   buildArgsWithDeprecations,
   renderedConnectorsFor,
 } from "discourse/lib/plugin-connectors";
+import GlimmerComponent from "discourse/components/glimmer";
+
 /**
    A plugin outlet is an extension point for templates where other templates can
    be inserted by plugins.
@@ -33,22 +35,13 @@ import {
    The list of disabled plugins is returned via the `Site` singleton.
 
 **/
-import Component from "@ember/component";
 
-export default Component.extend({
-  tagName: "",
-  connectorTagName: "",
-  connectors: null,
-
-  init() {
-    this._super(...arguments);
-    const name = this.name;
-    if (name) {
-      const args = buildArgsWithDeprecations(
-        this.args || {},
-        this.deprecatedArgs || {}
-      );
-      this.set("connectors", renderedConnectorsFor(name, args, this));
-    }
-  },
-});
+export default class extends GlimmerComponent {
+  get connectors() {
+    const args = buildArgsWithDeprecations(
+      this.args.args || {},
+      this.args.deprecatedArgs || {}
+    );
+    return renderedConnectorsFor(this.args.name, args, this);
+  }
+}
