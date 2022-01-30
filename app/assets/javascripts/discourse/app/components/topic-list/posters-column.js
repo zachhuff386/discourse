a9a203ed20fadbc585007c9f7e0c2de6fa4daa9f
David Taylor <david@taylorhq.com>
2022-01-30 18:06:22 +0000

    DEV: Render posters HTML in javascript
    This is faster than rendering in the HBS template

--- app/assets/javascripts/discourse/app/components/topic-list/posters-column.js
+++ app/assets/javascripts/discourse/app/components/topic-list/posters-column.js
@@ -1,3 +1,26 @@
-import templateOnly from "@ember/component/template-only";
+import GlimmerComponent from "discourse/components/glimmer";
+import { renderAvatar } from "discourse/helpers/user-avatar";
+import { htmlSafe } from "@ember/template";
+import { escapeExpression } from "discourse/lib/utilities";
 
-export default templateOnly();
+export default class PostersColumn extends GlimmerComponent {
+  get postersHtml() {
+    let html = "";
+    for (const poster of this.args.posters) {
+      if (poster.moreCount) {
+        html += `<a class="posters-more-count">${escapeExpression(
+          poster.moreCount
+        )}</a>`;
+      } else {
+        html += `<a href="${escapeExpression(
+          poster.user.path
+        )}" data-user-card="${escapeExpression(
+          poster.user.username
+        )}" class="${escapeExpression(poster.extraClasses)}">`;
+        html += renderAvatar(poster.user, { imageSize: "small" });
+        html += "</a>";
+      }
+    }
+    return htmlSafe(html);
+  }
+}
