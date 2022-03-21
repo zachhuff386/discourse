af567edb4e3798016ce489884ce6e78c58470b22
Loïc Guitaut <loic@discourse.org>
2022-03-21 15:28:52 +0100

    DEV: Upgrade to Rails 7
    This patch upgrades Rails to version 7.0.2.3.

--- config/routes.rb
+++ config/routes.rb
@@ -648,7 +648,7 @@ Discourse::Application.routes.draw do
       put "rebake"
       put "unhide"
       put "locked"
-      put "notice"
+      put "notice" => "posts#toggle_notice" # can’t use 'notice' with Rails 7.0+. Might be a bug
       get "replies"
       get "revisions/latest" => "posts#latest_revision"
       get "revisions/:revision" => "posts#revisions", constraints: { revision: /\d+/ }
