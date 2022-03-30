27d9705532e0e103d4bdac9786b2cddbc70910fb
Bianca Nenciu <nenciu.bianca@gmail.com>
2022-03-30 13:39:53 +0300

    DEV: Use UploadReference for Group

---
+++ db/migrate/20220330160754_copy_groups_uploads_to_upload_references.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class CopyGroupsUploadsToUploadReferences < ActiveRecord::Migration[6.1]
+  def up
+    execute <<~SQL
+      INSERT INTO upload_references(upload_id, target_type, target_id, created_at, updated_at)
+      SELECT flair_upload_id, 'Group', id, created_at, updated_at
+      FROM groups
+      WHERE flair_upload_id IS NOT NULL
+    SQL
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
