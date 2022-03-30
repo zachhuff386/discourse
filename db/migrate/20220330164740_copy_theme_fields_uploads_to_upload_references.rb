898b4a84e6aa9092276535e52abaf7b471f78307
Bianca Nenciu <nenciu.bianca@gmail.com>
2022-03-30 20:15:46 +0300

    DEV: Use UploadReference for ThemeField

---
+++ db/migrate/20220330164740_copy_theme_fields_uploads_to_upload_references.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class CopyThemeFieldsUploadsToUploadReferences < ActiveRecord::Migration[6.1]
+  def up
+    execute <<~SQL
+      INSERT INTO upload_references(upload_id, target_type, target_id, created_at, updated_at)
+      SELECT upload_id, 'ThemeField', id, created_at, updated_at
+      FROM theme_fields
+      WHERE type_id = 2
+    SQL
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
