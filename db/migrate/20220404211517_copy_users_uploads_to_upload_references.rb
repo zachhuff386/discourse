fbbd6c5da566e8411aa9ee762c8b7b82b739f842
Bianca Nenciu <nenciu.bianca@gmail.com>
2022-04-05 00:21:14 +0300

    DEV: Use UploadReference for User

---
+++ db/migrate/20220404211517_copy_users_uploads_to_upload_references.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class CopyUsersUploadsToUploadReferences < ActiveRecord::Migration[6.1]
+  def up
+    execute <<~SQL
+      INSERT INTO upload_references(upload_id, target_type, target_id, created_at, updated_at)
+      SELECT uploaded_avatar_id, 'User', id, created_at, updated_at
+      FROM users
+      WHERE uploaded_avatar_id IS NOT NULL
+    SQL
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
