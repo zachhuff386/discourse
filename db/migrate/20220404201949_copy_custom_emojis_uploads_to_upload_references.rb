7593a76a0ad67aed8ee3d2be2d984ea8b68b8ebf
Bianca Nenciu <nenciu.bianca@gmail.com>
2022-04-04 23:32:58 +0300

    DEV: Use UploadReference for CustomEmoji

---
+++ db/migrate/20220404201949_copy_custom_emojis_uploads_to_upload_references.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class CopyCustomEmojisUploadsToUploadReferences < ActiveRecord::Migration[6.1]
+  def up
+    execute <<~SQL
+      INSERT INTO upload_references(upload_id, target_type, target_id, created_at, updated_at)
+      SELECT upload_id, 'CustomEmoji', id, created_at, updated_at
+      FROM custom_emojis
+      WHERE upload_id IS NOT NULL
+    SQL
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
